8ab40c622a9034afdc63b36b94a8405b2113c02b
Hien <aspcorenn@gmail.com>
2021-04-09 18:31:23 +0700

    Update man hinh chinh

--- D10_TongHop/QLLoai.py
+++ D10_TongHop/QLLoai.py
@@ -1,13 +1,30 @@
 from tkinter import *
 from functools import partial
 
+
+def openThemLoai():
+    mhThemLoai = Toplevel(root)
+    mhThemLoai.title("Them loai")
+    Label(mhThemLoai, text="Ten loai").grid(row=0, column=0)
+    tenloai = StringVar()
+    Entry(mhThemLoai, textvariable=tenloai).grid(row=0, column=1)
+    Button(mhThemLoai, text="Them loai").grid(row=0, column=2)
+
+
 root = Tk()
-root.title("Quan ly Loai")
+root.title("Quan ly Hang hoa")
 root.geometry("350x300")
-Label(root,text="Ten loai").grid(row=0, column=0)
-tenloai = StringVar()
-Entry(root, textvariable=tenloai).grid(row=0, column=1)
-Button(root, text="Them loai").grid(row=0,column=2)
+
+# Xu ly menu
+xulyThemLoai = partial(openThemLoai)
+
+# Menu
+menubar = Menu(root)
+mnuHeThong = Menu(menubar, tearoff=0)
+mnuHeThong.add_command(label="Them loai", command=xulyThemLoai)
+menubar.add_cascade(label="Quan ly Loai", menu=mnuHeThong)
+
+root.config(menu=menubar)
 
 
 root.mainloop()
