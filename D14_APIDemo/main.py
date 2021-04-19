0534e1f50083bcf87b22b44b68e2303e3b2fd007
Hien <aspcorenn@gmail.com>
2021-04-19 20:52:25 +0700

    Done API

--- D14_APIDemo/main.py
+++ D14_APIDemo/main.py
@@ -1,6 +1,33 @@
 from fastapi import FastAPI
+from models.KhachHang import khach_hang
+import json
+import os
 
 app = FastAPI()
+
+@app.get("/khachhang/{maso}", tags=["customers"])
+def lay_khach_hang(maso):
+    filename = f"./data/{maso}.json"
+    if os.path.exists(filename):
+        with open(filename, "r") as myfile:
+            return {
+                "success": True,
+                "data": myfile.read()
+            }
+    return  {"success": False, "message":"Lỗi"}
+
+@app.post("/khachhang", tags=["customers"])
+def them_khach_hang(data : khach_hang):
+    mydata = {
+        "ma_so": data.ma_so,
+        "ma_so": data.ma_so,
+        "active": data.active,
+        "doanh_so" : data.doanh_so,
+        "email": data.email
+    }
+    jsonStr = json.dumps(mydata, indent=4)
+    with open(f"./data/{data.ma_so}.json", "w") as file:
+        file.write(jsonStr)
 
 @app.get("/")
 def read_root():
