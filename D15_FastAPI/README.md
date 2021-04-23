17622fdb4ed93209fe987f4b999e49ca17ef8d8e
Hien <aspcorenn@gmail.com>
2021-04-23 20:48:54 +0700

    Demo MySQL

--- D15_FastAPI/README.md
+++ D15_FastAPI/README.md
@@ -3,6 +3,7 @@ pip install fastapi
 pip install uvicorn[standard]
 pip install flake8  # Verify Code style
 pip install python-multipart
+pip install mysql-connector-python
 
 # Export to requirement.txt
 pip freeze > requirements.txt
--- D15_FastAPI/api.py
+++ D15_FastAPI/api.py
@@ -1,9 +1,10 @@
 # api.py
 from typing import List
-
+import mysql.connector
 from fastapi import FastAPI, File, Form, UploadFile
 import os
 import shutil
+from app.models.NguoiDungModel import NguoiDung
 
 
 app = FastAPI()
@@ -35,3 +36,26 @@ def upload_multi_file(images: List[UploadFile] = File(...)):
                                  image.filename)
         with open(full_path, "wb") as myfile:
             shutil.copyfileobj(image.file, myfile)
+
+
+@app.get("/user", tags=["Users"])
+def get_all_user():
+    # import mysql.connector
+    mydb = mysql.connector.connect(
+        host="sql6.freemysqlhosting.net",
+        user="sql6407749",
+        password="dMuLKDMrDB",
+        database="sql6407749"
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM nguoidung")
+    myresult = mycursor.fetchall()
+    users = []
+    for nd in myresult:
+        users.append(NguoiDung(
+            HoTen = nd[3],
+            Email = nd[4],
+            Username = nd[1]
+        ))
+
+    return users
