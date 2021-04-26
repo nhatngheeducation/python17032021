886e58b63cb8c4a6b7e8ee0de1c2b01a3378a822
Hien <aspcorenn@gmail.com>
2021-04-26 19:06:36 +0700

    Add schedule

---
+++ D17_ScrapeData/Demo_CaoGiaVang_PNJ.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def scrape_pnj_price():
+    pnjObj = requests.get("https://giavang.pnj.com.vn/")
+    textContent = pnjObj.text
+
+    soup = BeautifulSoup(textContent, features="html.parser")
+
+    tdgroups = soup.find_all('td')
+    data = []
+    for item in tdgroups:
+        print(item.text)
+        if item.get_attribute_list('align') == ['center'] and \
+            item.get_attribute_list('valign') == ['middle']:
+            data.append(item.text)
+
+    print(data)
+    # Process data nhận được
